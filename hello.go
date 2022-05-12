44b39e5c2a8907b83fef7518ed718fad1f76e56e
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-12 15:43:00 +0300

    Chapter 2. Anonymous function as result from other function

--- hello.go
+++ hello.go
@@ -3,11 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	action(10, 25, func(x int, y int) int { return x + y }) // 35
-	action(5, 6, func(x int, y int) int { return x * y })   // 30
+	fmt.Println("sum")
+	f := selectFn(1)
+	fmt.Println(f(2, 3)) // 5
+	fmt.Println(f(4, 5)) // 9
+	fmt.Println(f(6, 7)) // 13
+
+	fmt.Println("\nsubtraction")
+	f = selectFn(2)
+	fmt.Println(f(2, 3)) // -1
+	fmt.Println(f(7, 4)) // 3
+
+	fmt.Println("\nmultiply")
+	f = selectFn(5)
+	fmt.Println(f(2, 3)) // 6
+	fmt.Println(f(4, 5)) // 20
+	fmt.Println(f(6, 7)) // 42
 }
 
-func action(n1 int, n2 int, operation func(int, int) int) {
-	result := operation(n1, n2)
-	fmt.Println(result)
+func selectFn(n int) func(int, int) int {
+	if n == 1 {
+		return func(x int, y int) int { return x + y }
+	} else if n == 2 {
+		return func(x int, y int) int { return x - y }
+	} else {
+		return func(x int, y int) int { return x * y }
+	}
 }
