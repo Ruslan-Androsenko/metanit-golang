69e393fa96139f081ffc0deb88cf35fcadaaa3d7
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-12 15:48:15 +0300

    Chapter 2. Access to the environment

--- hello.go
+++ hello.go
@@ -3,30 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("sum")
-	f := selectFn(1)
-	fmt.Println(f(2, 3)) // 5
-	fmt.Println(f(4, 5)) // 9
-	fmt.Println(f(6, 7)) // 13
-
-	fmt.Println("\nsubtraction")
-	f = selectFn(2)
-	fmt.Println(f(2, 3)) // -1
-	fmt.Println(f(7, 4)) // 3
-
-	fmt.Println("\nmultiply")
-	f = selectFn(5)
-	fmt.Println(f(2, 3)) // 6
-	fmt.Println(f(4, 5)) // 20
-	fmt.Println(f(6, 7)) // 42
+	f := square()
+	fmt.Println(f()) // 9
+	fmt.Println(f()) // 16
+	fmt.Println(f()) // 25
 }
 
-func selectFn(n int) func(int, int) int {
-	if n == 1 {
-		return func(x int, y int) int { return x + y }
-	} else if n == 2 {
-		return func(x int, y int) int { return x - y }
-	} else {
-		return func(x int, y int) int { return x * y }
+func square() func() int {
+	var x int = 2
+	return func() int {
+		x++
+		return x * x
 	}
 }
