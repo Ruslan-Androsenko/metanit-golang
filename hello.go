4c2da4a9457c3abf9378126d9cc0403f044526dd
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-13 00:36:11 +0300

    Chapter 4. Methods by structure

--- hello.go
+++ hello.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
-type library []string
-
-func main() {
-	var lib library = library{"Book1", "Book2", "Book3"}
-	lib.print()
+type person struct {
+	name string
+	age  int
 }
 
-func (l library) print() {
-	for _, val := range l {
-		fmt.Println(val)
-	}
+func main() {
+	var tom = person{name: "Tom", age: 24}
+	tom.print()
+	tom.eat("борщ с капустой, но не красный")
+}
+
+func (p person) print() {
+	fmt.Println("Имя:", p.name)
+	fmt.Println("Возраст:", p.age)
+}
+
+func (p person) eat(meal string) {
+	fmt.Println(p.name, "ест", meal)
 }
