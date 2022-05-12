f8882dd1b1da2c718ed52b5b029716b7782ff1a5
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-12 14:39:06 +0300

    Chapter 2. Conditional constructs. switch

--- hello.go
+++ hello.go
@@ -3,24 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	a := 6
-	b := 7
+	a := 87
 
-	if a < b {
-		fmt.Println("a is less than b")
-	} else {
-		fmt.Println("a is more than b")
-	}
-
-	fmt.Println()
-	a = 8
-	b = 8
-
-	if a < b {
-		fmt.Println("a меньше b")
-	} else if a > b {
-		fmt.Println("a больше b")
-	} else {
-		fmt.Println("a равно b")
+	switch a {
+	case 9:
+		fmt.Println("a = 9")
+	case 8:
+		fmt.Println("a = 8")
+	case 7:
+		fmt.Println("a = 7")
+	case 6, 5, 4:
+		fmt.Println("a = 6 или 5 или 4, но это не точно")
+	default:
+		fmt.Println("значение переменной a не определено")
 	}
 }
