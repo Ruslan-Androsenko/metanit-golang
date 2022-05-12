2a21086f24b511af07362e1e95a2295c2ec8d649
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-12 23:27:30 +0300

    Chapter 4. Declaring custom types

--- Readme.md
+++ Readme.md
@@ -25,5 +25,6 @@ go build hello.go
 
 # Изучаемые разделы учебника на Metanit.com
 
-### Глава 1. Введение в язык Go [Первая программа](https://metanit.com/go/tutorial/1.2.php)
+### Глава 4. Производные типы 
+[Объявление типов](https://metanit.com/go/tutorial/4.1.php)
 
--- hello.go
+++ hello.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type mile int
+type kilometer int
+type library []string
+
 func main() {
-	fmt.Println("Hello Go!")
+	fmt.Println("Type mile")
+	var distance mile = 5
+	distanceToEnemy(distance)
+
+	//var distance2 kilometer = 5
+	//distanceToEnemy(distance2)	! ошибка, передан аргумент другого типа
+
+	fmt.Println("\nType library")
+	var myLibrary library = library{"Book1", "Book2", "Book3"}
+	printBooks(myLibrary)
+}
+
+func distanceToEnemy(distance mile) {
+	fmt.Println("Расстояние до противника:")
+	fmt.Println(distance, "миль")
+}
+
+func printBooks(lib library) {
+	for _, value := range lib {
+		fmt.Println(value)
+	}
 }
