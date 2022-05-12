817936b7f550c39ac2c2a50115d4219383acc2ae
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-12 14:51:16 +0300

    Chapter 2. Functions as parameters to other functions

--- hello.go
+++ hello.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// var f func(int, int) int = add
-	f := add
-	fmt.Println("sum:", f(3, 4))
+	action(10, 25, add)    // 35
+	action(5, 6, multiply) // 30
 
-	f = multiply
-	fmt.Println("multiply:", f(3, 4))
+	slice := []int{-2, 4, 3, -1, 7, -4, 23}
 
-	var f1 func(string) = display
-	f1("hello")
+	sumOfEvens := sum(slice, isEven) // Сумма четных чисел
+	fmt.Println(sumOfEvens)          // -2
+
+	sumOfPositive := sum(slice, isPositive) // Сумма положительных чисел
+	fmt.Println(sumOfPositive)              // 37
 }
 
 func add(x int, y int) int {
@@ -22,6 +23,27 @@ func multiply(x int, y int) int {
 	return x * y
 }
 
-func display(message string) {
-	fmt.Println(message)
+func action(n1 int, n2 int, operation func(int, int) int) {
+	result := operation(n1, n2)
+	fmt.Println(result)
+}
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+func isPositive(n int) bool {
+	return n > 0
+}
+
+func sum(numbers []int, criteria func(int) bool) int {
+	result := 0
+
+	for _, val := range numbers {
+		if criteria(val) {
+			result += val
+		}
+	}
+
+	return result
 }
