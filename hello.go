c67e5ae8e32a7215d562815ae06f63828c86c0f6
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-13 00:16:09 +0300

    Chapter 4. Storing a reference to a structure of the same type

--- hello.go
+++ hello.go
@@ -2,28 +2,32 @@ package main
 
 import "fmt"
 
-type contact struct {
-	email string
-	phone string
-}
-
-type person struct {
-	name string
-	age  int
-	contact
+type node struct {
+	value int
+	next  *node
 }
 
 func main() {
-	var tom = person{
-		name: "Tom",
-		age:  24,
-		contact: contact{
-			email: "tom@gmail.com",
-			phone: "+1234567899",
-		},
-	}
-	tom.email = "supertom@gmail.com"
+	first := node{value: 4}
+	second := node{value: 5}
+	third := node{value: 6}
 
-	fmt.Println(tom.email)
-	fmt.Println(tom.phone)
+	first.next = &second
+	second.next = &third
+
+	var current *node = &first
+
+	for current != nil {
+		fmt.Println(current.value)
+		current = current.next
+	}
+}
+
+// Рекурсивный вывод списка
+func printNodeValue(n *node) {
+	fmt.Println(n.value)
+
+	if n.next != nil {
+		printNodeValue(n.next)
+	}
 }
