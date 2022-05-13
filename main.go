df80a1677b87eb4f497a2b1f429efc2ec84874e4
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-13 10:28:41 +0300

    Chapter 5. Loading and connecting an external module

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"hello/computation"
+	"rsc.io/quote"
 )
 
 func main() {
+	message := quote.Hello()
+	fmt.Println(message)
+
+	fmt.Println()
 	fmt.Println(computation.Factorial(4))
 	fmt.Println(computation.Factorial(5))
 }
