b936d01874111945fc516c7b8ce13db1cf365593
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2022-05-18 21:47:49 +0300

    Chapter 8. io.Writer

--- main.go
+++ main.go
@@ -2,33 +2,30 @@ package main
 
 import (
 	"fmt"
-	"io"
 )
 
-type phoneReader string
+type phoneWriter struct{}
 
-func (ph phoneReader) Read(p []byte) (int, error) {
-	count := 0
+func (p phoneWriter) Write(bs []byte) (int, error) {
+	if len(bs) == 0 {
+		return 0, nil
+	}
 
-	for i := 0; i < len(ph); i++ {
-		if ph[i] >= '0' && ph[i] <= '9' {
-			p[count] = ph[i]
-			count++
+	for i := 0; i < len(bs); i++ {
+		if bs[i] >= '0' && bs[i] <= '9' {
+			fmt.Print(string(bs[i]))
 		}
 	}
 
-	return count, io.EOF
+	fmt.Println()
+	return len(bs), nil
 }
 
 func main() {
-	phone1 := phoneReader("+1(234)567 9010")
-	phone2 := phoneReader("+2-345-678-12-35")
+	bytes1 := []byte("+1(234)567 9010")
+	bytes2 := []byte("+2-345-678-12-35")
 
-	buffer := make([]byte, len(phone1))
-	phone1.Read(buffer)
-	fmt.Println(string(buffer)) // 12345679010
-
-	buffer = make([]byte, len(phone2))
-	phone2.Read(buffer)
-	fmt.Println(string(buffer)) // 23456781235
+	writer := phoneWriter{}
+	writer.Write(bytes1)
+	writer.Write(bytes2)
 }
